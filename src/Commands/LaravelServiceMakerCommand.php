214ac0aa805fc01e81ad0c1f8283cc241700102f
Rémi Latrille <remi.latrille@gmail.com>
2022-07-23 03:01:36 +0200

    fix flag option condition

--- src/Commands/LaravelServiceMakerCommand.php
+++ src/Commands/LaravelServiceMakerCommand.php
@@ -29,7 +29,7 @@ class LaravelServiceMakerCommand extends Command
 
         ]);
 
-        if (config('service-maker.with_interface') || $this->option('noContract')) {
+        if (config('service-maker.with_interface') || !$this->option('noContract')) {
             $this->call('make:servicecontractfile', [
                 'name' => $this->argument('name'),
             ]);
