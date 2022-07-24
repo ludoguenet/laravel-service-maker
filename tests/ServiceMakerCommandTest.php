59e27e6a33067d417262a5a763b5676fb33e750c
ludoguenet <ludovicguenet@gmx.com>
2022-07-24 15:52:54 +0200

    Test all commands are successful

--- tests/ServiceMakerCommandTest.php
+++ tests/ServiceMakerCommandTest.php
@@ -5,3 +5,19 @@ use function Pest\Laravel\artisan;
 it('returns a successful response when make:service command is executed', function () {
     artisan('make:service CreateUser')->assertExitCode(1);
 });
+
+it('returns a successful response when make:service command is executed with option', function () {
+    artisan('make:service CreateUser --noContract')->assertExitCode(1);
+});
+
+it('is successful when make:servicecontractfile command is executed', function () {
+    artisan('make:servicecontractfile CreateUser')->assertSuccessful();
+});
+
+it('is successful when make:servicefile command is executed', function () {
+    artisan('make:servicefile CreateUser')->assertSuccessful();
+});
+
+it('is successful when make:servicewithnocontract command is executed', function () {
+    artisan('make:servicewithnocontract CreateUser')->assertSuccessful();
+});
